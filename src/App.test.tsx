814279247a6da54ad23676e1dd17b95c6477890a
Soobin Son <ssbin93@gmail.com>
2023-07-18 23:29:14 +1000

    update App test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,23 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe("App component", () => {
+  it("renders a App component", () => {
+    const { container } = render(<App />);
+    expect(container).toBeInTheDocument();
+  });
+
   it("renders a heading element", () => {
     render(<App />);
     const headingElement = screen.getByText(
-      /Shall we find out what movie genres out there/i
+      /shall we find out what movie genres out there/i
     );
     expect(headingElement).toBeInTheDocument();
+  });
+
+  it("renders a load button", () => {
+    render(<App />);
+    const buttonElement = screen.getByText(/load movie genres/i);
+    expect(buttonElement).toBeInTheDocument();
   });
 });
 
