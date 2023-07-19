bc0c9fadd9914a2b02651d201a9efaea5914224a
Soobin Son <ssbin93@gmail.com>
2023-07-19 23:03:18 +1000

    update use-fetch test file

--- src/features/hooks/use-fetch/use-fetch.test.ts
+++ src/features/hooks/use-fetch/use-fetch.test.ts
@@ -1,30 +1,57 @@
 import { renderHook, act } from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from 'msw/node'
+
 import { useFetch } from "./use-fetch"
 
-describe("useFetch", () => {
-  it("should initialize with null data and false error", () => {
+const MOCK_GENRES = [
+  {
+    id: 1,
+    name: 'Action'
+  }, {
+    id: 2,
+    name: 'Fiction'
+  }
+];
+
+describe("useFetch hook", () => {
+  const mockServer = setupServer(
+    rest.get('/movie-genres', (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          genres: MOCK_GENRES
+        })
+      );
+    })
+  );
+
+  beforeAll(() => {
+    mockServer.listen();
+  });
+
+  afterAll(() => {
+    mockServer.close();
+  })
+
+  it("should initialize with default values", () => {
     const { result } = renderHook(() =>
-      useFetch("https://api.example.com/data", "my-token")
+      useFetch("/movie-genres", "my-token")
     );
-    console.log(result.current);
     expect(result.current.data).toBe(undefined);
     expect(result.current.error).toBe(false);
   });
 
-  it("should call fetch with the correct parameters", async () => {
-    global.fetch = jest
-      .fn()
-      .mockResolvedValueOnce({
-        json: jest.fn().mockResolvedValueOnce({ foo: "bar" }),
-      });
+  it("should call fetch with the correct token", async () => {
+    global.fetch = jest.fn();
 
     const { result } = renderHook(() =>
-      useFetch("https://api.example.com/data", "my-token")
+      useFetch("/movie-genres", "my-token")
     );
 
     await act(() => result.current.fire());
 
-    expect(global.fetch).toHaveBeenCalledWith("https://api.example.com/data", {
+    expect(global.fetch).toHaveBeenCalledWith("/movie-genres", {
       method: "GET",
       headers: {
         accept: "application/json",
@@ -33,27 +60,26 @@ describe("useFetch", () => {
     });
   });
 
-  it("should update data when fetch response is successful", async () => {
+  it("should receive the correct response", async () => {
     global.fetch = jest
       .fn()
       .mockResolvedValueOnce({
-        json: jest.fn().mockResolvedValueOnce({ foo: "bar" }),
+        json: jest.fn().mockResolvedValueOnce({ genres: MOCK_GENRES }),
       });
 
     const { result } = renderHook(() =>
-      useFetch("https://api.example.com/data", "my-token")
+      useFetch("/movie-genres", "my-token")
     );
 
     await act(() => result.current.fire());
-
-    expect(result.current.data).toEqual({ foo: "bar" });
+    expect(result.current.data).toEqual({ genres: MOCK_GENRES })
   });
 
   it("should set error to true when fetch response fails", async () => {
     global.fetch = jest.fn().mockRejectedValueOnce(new Error("Fetch failed"));
 
     const { result } = renderHook(() =>
-      useFetch("https://api.example.com/data", "my-token")
+      useFetch("/movie-genres", "my-token")
     );
 
     await act(() => result.current.fire());
